9d2e57afd64cd4cfff5cb058e681b71ef8f678fc
roj08 <rociojuamperez@gmail.com>
2022-05-03 12:42:21 -0300

    contador

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import './App.css';
 import React from 'react';
 import NavBar from './components/NavBar.js';
 import Titulo from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
+import swal from 'sweetalert';
 
 export default function App() {
+
+  const onAdd = (qty) => {
+    swal({
+      title: "Completado",
+      text: "Agregaste " + qty + " productos",
+      icon: "success",
+    });
+  };
+
   return (
     <>
       <NavBar />
       <Titulo titulo="Cositas Bonitas" />
+      <ItemCount onAdd={onAdd} />
     </>
   );
 }
